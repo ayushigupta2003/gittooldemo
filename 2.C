e3b66dcef1457ff3af9bf1575890b337a6f98827
unknown <aushigga@gmail.com>
2022-12-10 15:57:41 +0530

    update 2.c

--- 2.C
+++ 2.C
@@ -1,18 +1,6 @@
    void main()
    {
-    int i,j,n=5;
-    clrscr();
-    for(i=1;i<=n;i++)
-    {
-     for(j=n;j>=1;j--)
-     {
-     if(j<n-i)
-     printf(" ");
-     else
-     printf("%d",j);
-     }
-     printf("\n");
-     }
+    printf("git");
      getch();
       }
 
