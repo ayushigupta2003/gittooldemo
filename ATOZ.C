3ef874a805c70ee6ffb217573b6a5ea3d9aa350c
unknown <aushigga@gmail.com>
2022-12-10 15:15:36 +0530

    new file added

---
+++ ATOZ.C
@@ -0,0 +1,201 @@
+/* void main()
+ {
+  int i,j,n=5;
+  clrscr();
+  for(i=0;i<n;i++)
+  {
+   for(j=0;j<n;j++)
+     {
+   if(j==0||i==0||j==n-1||i==n/2)         //a
+      {
+    printf("*");
+       }
+    else
+      {
+    printf(" "); }
+
+    }
+    printf("\n");
+    }
+   getch();
+   } */
+
+
+  /*  void main()
+ {
+  int i,j,n=7;
+  clrscr();
+  for(i=0;i<n;i++)
+  {
+   for(j=0;j<n;j++)
+     {
+   if(j==0||i==0||i==n-1||i==n/2||j==n-1)
+      {
+    printf("*");
+       }
+    else                                      //b
+      {
+    printf(" "); }
+
+    }
+    printf("\n");
+    }
+   getch();
+   }  */
+
+
+  /* void main()
+ {
+  int i,j,n=5;
+  clrscr();
+  for(i=0;i<n;i++)
+  {
+   for(j=0;j<n;j++)
+     {
+   if(j==0||i==0||i==n-1)
+      {
+    printf("*");                                 //c
+       }
+    else
+      {
+    printf(" "); }
+
+    }
+    printf("\n");
+    }
+   getch();
+   } */
+
+
+/*  void main()
+ {
+  int i,j,n=5;
+  clrscr();
+  for(i=0;i<n;i++)
+  {
+   for(j=0;j<n;j++)
+     {
+   if(j==1||i==0||j==n-1||i==n-1)                   //d
+      {
+    printf("*");
+       }
+    else
+      {
+    printf(" "); }
+
+    }
+    printf("\n");
+    }
+   getch();
+   } */
+
+ /*  void main()
+ {
+  int i,j,n=5;
+  clrscr();
+  for(i=0;i<n;i++)                       //e
+  {
+   for(j=0;j<n;j++)
+     {
+   if(j==0||i==0||i==n/2||i==n-1)
+      {
+    printf("*");
+       }
+    else
+      {
+    printf(" "); }
+
+    }
+    printf("\n");
+    }
+   getch();
+   }   */
+
+ /*  void main()
+ {
+  int i,j,n=5;
+  clrscr();
+  for(i=0;i<n;i++)
+  {                                        //f
+   for(j=0;j<n;j++)
+     {
+   if(j==0||i==0||i==n/2||i==n/2)
+      {
+    printf("*");
+       }
+    else
+      {
+    printf(" "); }
+
+    }
+    printf("\n");
+    }
+   getch();
+   } */
+
+
+
+ /* void main()
+ {
+  int i,j,n=5;
+  clrscr();
+  for(i=0;i<=n;i++)
+  {
+   for(j=0;j<=n;j++)
+     {
+      if(j==0||i==n/2||j==n)             //h
+      {
+    printf("*");
+       }
+    else
+      {
+    printf(" "); }
+
+    }
+    printf("\n");
+    }
+   getch();
+   }*/
+
+/*void main()
+ {
+  int i,j,n=5;
+  clrscr();
+  for(i=0;i<=n;i++)                         //i
+  {
+   for(j=0;j<=n;j++)
+     {
+      if(i==0||i==n||j==n/2)
+      {
+    printf("*");
+       }
+    else
+      {
+    printf(" "); }
+
+    }
+    printf("\n");
+    }
+   getch();
+   }*/
+  /*  void main()
+ {
+  int i,j,n=5;
+  clrscr();
+  for(i=0;i<=n;i++)
+  {
+   for(j=0;j<=n;j++)
+     {
+      if(i==0||j==n/2+1||(i==n&&j==n/n)||(i==n&&j==0)||(j==0&&i==n-1)||(i==n&&j==n/2))
+      {
+    printf("*");
+       }
+    else
+      {
+    printf(" "); }
+
+    }
+    printf("\n");                   //j
+    }
+   getch();
+   }*/
